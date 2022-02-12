5b2bea9c1361e4bfb779eebf82efad366339545e
Grace Waceke <gracewacekemwangi@gmail.com>
2022-02-12 12:56:00 +0300

    first commit

---
+++ src/main/kotlin/Grace.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var name = "Grace"
+    println(name)
+
+    var age = "22"
+    println(age)
+
+    var phonenumber = "0743800006"
+    println(phonenumber)
+
+    var weight = "60"
+    println(weight)
+
+    val isKenyan = true
+    println(isKenyan)
+
+    val isnotKenyan = false
+    println(isnotKenyan)
+
+
+}
